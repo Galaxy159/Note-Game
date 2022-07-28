3f181af3ac1be1e2b3192d59e52fcae8cc81748f
Galaxy159 <88257961+Galaxy159@users.noreply.github.com>
2022-07-28 21:36:29 +0300

    Add files via upload

--- scripten.js
+++ scripten.js
@@ -467,6 +467,47 @@ const updateStats = function () {
   }`;
 };
 
+////////////////////////////////////////////////////////
+// Audio API
+const notes = [
+  { name: "E", frequency: 82 },
+  { name: "F", frequency: 87 },
+  { name: "G", frequency: 98 },
+  { name: "A", frequency: 110 },
+  { name: "B", frequency: 123 },
+  { name: "C", frequency: 131 },
+  { name: "D", frequency: 147 },
+  { name: "E", frequency: 165 },
+  { name: "F", frequency: 175 },
+  { name: "G", frequency: 196 },
+  { name: "A", frequency: 220 },
+  { name: "B", frequency: 247 },
+  { name: "C", frequency: 262 },
+  { name: "D", frequency: 294 },
+  { name: "E", frequency: 330 },
+  { name: "F", frequency: 349 },
+  { name: "G", frequency: 392 },
+  { name: "A", frequency: 440 },
+  { name: "B", frequency: 494 },
+  { name: "C", frequency: 523 },
+  { name: "D", frequency: 587 },
+];
+
+const audioContext = new AudioContext();
+const primaryGainControl = audioContext.createGain();
+primaryGainControl.gain.setValueAtTime(0.2, 0);
+primaryGainControl.connect(audioContext.destination);
+
+const playNote = function (randomNote) {
+  const noteOscillator = audioContext.createOscillator();
+  const frequency = notes[randomNote - 1].frequency;
+  noteOscillator.type = "sine";
+  noteOscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
+  noteOscillator.connect(primaryGainControl);
+  noteOscillator.start();
+  noteOscillator.stop(audioContext.currentTime + 0.5);
+};
+
 /////////////////////////////////////////////////////
 // Feedback functions
 const generateCorrectImg = function () {
@@ -486,7 +527,8 @@ const generateWrongImg = function () {
 };
 
 const feedbackCorrect = function () {
-  correctSound.play();
+  playNote(randomNote);
+  // correctSound.play();
   generateCorrectImg();
   setTimeout(() => {}, 500);
 };
