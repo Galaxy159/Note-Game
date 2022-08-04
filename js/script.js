2d69d8b11e979d3dfa5239bf42b07cb6641d0d09
Galaxy159 <88257961+Galaxy159@users.noreply.github.com>
2022-08-04 17:14:17 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -487,3 +487,15 @@ names.forEach((name) =>
     }
   })
 );
+
+///////////////////////////////////////////
+// Microphone permission
+
+navigator.mediaDevices
+  .getUserMedia({ audio: true })
+  .then(function (stream) {
+    console.log("You let me use your mic!");
+  })
+  .catch(function (err) {
+    console.log("No mic for you!");
+  });
